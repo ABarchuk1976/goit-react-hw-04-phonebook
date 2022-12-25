52acdadd0ce422255a4ed06174069da711fd9327
Barchuk Andrii <abarchuk1976@gmail.com>
2022-12-25 11:12:10 +0200

    Fixed ClientList and Filter are functional components

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,33 +1,23 @@
-import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { FilterLabel, FilterInput } from './Filter.styled';
 
-class Filter extends Component {
-  static propTypes = {
-    value: PropTypes.string,
-    onChange: PropTypes.func,
-  };
+const Filter = ({ value, onChange }) => (
+  <FilterLabel>
+    Find contacts by name
+    <FilterInput
+      type="text"
+      name="filter"
+      title="Filter is case unsensitive."
+      value={value}
+      onChange={onChange}
+      required
+    />
+  </FilterLabel>
+);
 
-  // handlerClick = evt => {
-  //   this.props.onChange(evt.target.value);
-  // };
-
-  render() {
-    const { value, onChange } = this.props;
-    return (
-      <FilterLabel>
-        Find contacts by name
-        <FilterInput
-          type="text"
-          name="filter"
-          title="Filter is case unsensitive."
-          value={value}
-          onChange={onChange}
-          required
-        />
-      </FilterLabel>
-    );
-  }
-}
+Filter.propTypes = {
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+};
 
 export default Filter;
